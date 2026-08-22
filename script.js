066d0f7c137f9a5001d835631a92e2ac94e00b21
xandue077 <kangking1903@gmail.com>
2026-08-22 10:17:39 +0700

    Add initial configuration for TTS OP BOT

---
+++ script.js
@@ -0,0 +1,10 @@
+{
+  name: "TTS OP BOT",
+  description: "TEXT TO SPEAK BOT. HAVE 4 LANGUAGE TO SPEAK. IT HAS ENGLISH, KHMER, INDONESIA. PHILIPPINES.",
+  category: "Utility",
+  icon: "💀",
+  color: "#5865f2",
+  tags: ["Tools", "Commands"],
+  clientId: "1518895458083143690",
+  permissions: "2147483648"
+}
